a3a6add80f361d9e428b06d9a5d69e05347dc141
wisdomdzontoh <wisdomdzontoh@gmail.com>
2024-08-27 00:11:32 +0000

    updated application form

--- application_portal/urls.py
+++ application_portal/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('job-applications/', views.application_list, name='job-applications'),
     path('application-portal/', views.application_portal, name='application-portal'),
-    path('application-form/', views.application_form, name='application-form'),
+    path('application-portal/form', views.application_form, name='application-form'),
     
     
     # other paths...
