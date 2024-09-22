3a1d3601416262360d37d3fa7ab9a347ec431139
wisdomdzontoh <wisdomdzontoh@gmail.com>
2024-09-22 23:26:28 +0000

    updated settings.py

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -126,14 +126,14 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 
 import environ
 
