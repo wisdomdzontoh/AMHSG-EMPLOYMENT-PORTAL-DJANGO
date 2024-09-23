5c5dab64ffe841407768d3bb4c2ec8526afc6b91
wisdomdzontoh <wisdomdzontoh@gmail.com>
2024-09-23 00:39:29 +0000

    deployment bug fix

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -30,7 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = os.environ.get('SECRET_KEY', '')
-SECRETE_KEY='django-insecure-q%kkrpi!o9xsz85ur!aoxth06fwye6bgjdlp40$%0hvo9rg163'
+SECRETE_KEY= 'django-insecure-q%kkrpi!o9xsz85ur!aoxth06fwye6bgjdlp40$%0hvo9rg163'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
