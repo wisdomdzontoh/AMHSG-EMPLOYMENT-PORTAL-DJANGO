c316b1edecb699a6bf9383fbbf7ac5e63d94d032
wisdomdzontoh <wisdomdzontoh@gmail.com>
2024-09-23 12:42:50 +0000

    updates to secrete key

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = os.environ.get('SECRET_KEY', '')
-SECRETE_KEY= 'ho)3&$h+^^#su@@0dn6-n2wny!*!im4v!dy_ryzg^&fflc+6$x'
+SECRET_KEY= 'django-insecure-q%kkrpi!o9xsz85ur!aoxth06fwye6bgjdlp40$%0hvo9rg163'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
