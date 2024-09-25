4d16d788b4818efc012ae08f7eec99ffe178f4dd
wisdomdzontoh <wisdomdzontoh@gmail.com>
2024-09-25 06:51:59 +0000

    updates

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -131,14 +131,14 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 
 
 
@@ -146,11 +146,12 @@ DATABASES = {
 
 
 #deployment database
-import dj_database_url
+
+"""import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
-}
+}"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
